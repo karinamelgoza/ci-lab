709e9c84d0c4ddf20dd24a9fa9b052f5c7fdb56e
Karina Melgoza <kmelgoza08@gmail.com>
2021-06-28 14:18:40 -0700

    adds test 2

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test('test 2', () => {
+  expect(getGreeting('Karina')).toBe("Hi Karina")
+})
